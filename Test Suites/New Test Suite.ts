069f0b977f61a40d843dd9360361294afb96b838
madhumitha.s <madhumitha.s@inbl01lap7054.brd.g-c-i.com>
2023-07-26 16:01:27 +0530

    1st commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6ed9e9e2-e12f-46a2-8283-ac140fd0692c</testSuiteGuid>
+</TestSuiteEntity>
